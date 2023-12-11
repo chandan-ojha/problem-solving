93cade4a9ba3a6f70456d7fecc5ecdc325a6985e
Chandan Ojha <chandanojha776@gmail.com>
2023-12-11 11:27:55 +0600

    `customer-who-visited-but-did-not-make-any-transactions`

---
+++ leetcode/sql/8.customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+#solution
+
+SELECT  Visits.customer_id,COUNT(Visits.customer_id) AS count_no_trans 
+FROM Visits 
+LEFT JOIN Transactions ON Visits.visit_id = Transactions.visit_id
+WHERE Transactions.visit_id IS NULL
+GROUP BY Visits.customer_id
