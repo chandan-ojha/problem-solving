84279f88089c0afce9c96e25fe95ee01f9d95505
Chandan Ojha <chandanojha776@gmail.com>
2023-12-11 12:01:57 +0600

    `average-selling-price`

---
+++ leetcode/sql/16.average-selling-price.sql
@@ -0,0 +1,12 @@
+#solution
+
+SELECT
+    p.product_id,
+    ROUND(COALESCE(SUM(p.price * us.units) / NULLIF(SUM(us.units), 0), 0), 2) AS average_price
+FROM
+    Prices p
+LEFT JOIN
+    UnitsSold us ON p.product_id = us.product_id
+           AND us.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY
+    p.product_id;
