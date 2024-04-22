2c1d2b868d0c2a1ead0bb931dea90bea69d790b7
Chandan Ojha <chandanojha776@gmail.com>
2024-04-22 16:45:21 +0600

    #D_Strings

---
+++ codeforces/c/D_Strings.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[11], b[11];
+    scanf("%s", a);
+    scanf("%s", b);
+
+    int al = strlen(a);
+    int bl = strlen(b);
+
+    printf("%d %d\n", al, bl);
+
+    char c[al + bl];
+
+    for (int i = 0; i <= 11; i++)
+    {
+        c[i] = a[i];
+    }
+
+    int ual = al;
+
+    for (int i = 0; i <= 11; i++)
+    {
+        c[ual] = b[i];
+        ual++;
+    }
+
+    printf("%s\n", c);
+
+    int temp = a[0];
+
+    a[0] = b[0];
+
+    b[0] = temp;
+
+    printf("%s %s", a, b);
+
+    return 0;
+}
