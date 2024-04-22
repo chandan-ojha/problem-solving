17118574bf5c33d855f0b6f5bbce75cf1ea43667
Chandan Ojha <chandanojha776@gmail.com>
2024-04-22 16:21:19 +0600

    #todo

---
+++ codeforces/c/C_Compare.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char x[21], y[21];
+
+    scanf("%s", x);
+    scanf("%s", y);
+
+    int xl = strlen(x);
+    int yl = strlen(y);
+
+    if (xl <= yl)
+    {
+        printf("%s\n", x);
+    }
+    else
+    {
+        printf("%s\n", y);
+    }
+
+    return 0;
+}
