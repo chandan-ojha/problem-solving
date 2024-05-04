359eb5ab5b37ac56b2acc144a94c975e45facaf9
Chandan Ojha <chandanojha776@gmail.com>
2024-05-04 10:21:54 +0600

    #E_Swap solved

---
+++ codeforces/assiut-university-training-newcomers/E_Swap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+
+    return;
+}
+
+int main()
+{
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    swap(&x, &y);
+
+    printf("%d %d", x, y);
+
+    return 0;
+}
