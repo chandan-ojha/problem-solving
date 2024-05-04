d6d518f2a3c35df1efe950cfafcce7741e3e8217
Chandan Ojha <chandanojha776@gmail.com>
2024-05-04 11:48:38 +0600

    #H_N_Times

---
+++ codeforces/assiut-university-training-newcomers/H_N_Times.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void n_times()
+{
+    int n;
+    scanf("%d", &n);
+    getchar();
+    char ch;
+    scanf("%c", &ch);
+    getchar();
+
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%c ", ch);
+    }
+
+    return;
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        n_times();
+        printf("\n");
+    }
+    
+
+    return 0;
+}
