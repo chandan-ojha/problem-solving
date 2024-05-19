295916c43252000882ab3d0447a40a8f66703e41
Chandan Ojha <chandanojha776@gmail.com>
2024-05-19 23:10:35 +0600

    #Algomaniac_Finals solved

---
+++ codechef/contests-problems/Algomaniac_Finals.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 17)
+        cout << "YAY\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
