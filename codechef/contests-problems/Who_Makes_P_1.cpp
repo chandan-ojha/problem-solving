04f453c2630e2271ce941132effcb799fb9bf50e
Chandan Ojha <chandanojha776@gmail.com>
2024-05-29 21:09:55 +0600

    Create Who_Makes_P_1.cpp

---
+++ codechef/contests-problems/Who_Makes_P_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (b >= a)
+    {
+        cout << "Tyro" << endl;
+    }
+    else
+    {
+        cout << "Dom" << endl;
+    }
+
+    return 0;
+}
