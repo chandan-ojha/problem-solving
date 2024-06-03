9617e968d17cf8f82dc493155631daec44fc46d2
Chandan Ojha <chandanojha776@gmail.com>
2024-06-03 14:08:15 +0600

    Create Basic_Data_Types.cpp

---
+++ hackerrank/c++/Basic_Data_Types.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    long long int b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+
+    cout << a << endl
+         << b << endl
+         << c << endl
+         << fixed << setprecision(3)
+         << d << endl
+         << fixed << setprecision(9)
+         << e << endl;
+
+    return 0;
+}
