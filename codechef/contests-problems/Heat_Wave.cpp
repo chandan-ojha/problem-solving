fe9890abbcd1642300299d65efb63164aa0e2cd9
Chandan Ojha <chandanojha776@gmail.com>
2024-06-12 21:44:03 +0600

    Create Heat_Wave.cpp

---
+++ codechef/contests-problems/Heat_Wave.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+
+    if (x >= y)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+    return 0;
+}
